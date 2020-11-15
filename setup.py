fe49ad00e2ec551e5c8fc278791cd1dbb47b68f6
Ben Dilday <ben.dilday.phd@gmail.com>
2020-11-15 16:14:38 -0500

    typos

--- setup.py
+++ setup.py
@@ -4,12 +4,10 @@
 
 from setuptools import setup, find_packages
 
-with open("README.rst") as readme_file:
+with open("README.md") as readme_file:
     readme = readme_file.read()
 
-with open("HISTORY.rst") as history_file:
-    history = history_file.read()
-
+history = ''
 requirements = []
 
 setup_requirements = [
@@ -17,20 +15,19 @@ setup_requirements = [
 ]
 
 test_requirements = [
-    "pytest>=3",
+    "pytest>=6",
 ]
 
 setup(
     author="Ben Dilday",
     author_email="ben.dilday.phd@gmail.com",
-    python_requires=">=3.5",
+    python_requires=">=3.6",
     classifiers=[
         "Development Status :: 2 - Pre-Alpha",
         "Intended Audience :: Developers",
         "License :: OSI Approved :: MIT License",
         "Natural Language :: English",
         "Programming Language :: Python :: 3",
-        "Programming Language :: Python :: 3.5",
         "Programming Language :: Python :: 3.6",
         "Programming Language :: Python :: 3.7",
         "Programming Language :: Python :: 3.8",
