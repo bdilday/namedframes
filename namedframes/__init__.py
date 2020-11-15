4b173c316b62db0c279aedce3016f2757203bcc8
Ben Dilday <ben.dilday.phd@gmail.com>
2020-11-15 16:19:26 -0500

    temporary disable spark

--- namedframes/__init__.py
+++ namedframes/__init__.py
@@ -4,5 +4,4 @@ __author__ = """Ben Dilday"""
 __email__ = "ben.dilday.phd@gmail.com"
 __version__ = "0.1.0"
 
-from .spark_frames import NamedSparkDataFrame
 from .pandas_frames import NamedPandasDataFrame
