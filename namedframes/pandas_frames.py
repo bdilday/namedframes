9ea42d45805f5b64fb7fdf94720e6d72e3087a8f
Ben Dilday <ben.dilday.phd@gmail.com>
2020-11-28 19:56:11 -0500

    adds repr

--- namedframes/pandas_frames.py
+++ namedframes/pandas_frames.py
@@ -8,3 +8,12 @@ class PandasNamedFrame(PandasDataFrame, ValidateMixIn):
     def __init__(self, pandas_df):
         super().__init__(pandas_df)
         self._validate()
+
+    def __repr__(self):
+        message = "DataFrame with columns:\n"
+        column_descriptions = [
+            f"{column_name}: {column_type}"
+            for column_name, column_type in self.__annotations__.items()
+        ]
+        message += "\n".join(column_descriptions)
+        return message + "\n" + super().__repr__()
--- namedframes/spark_frames.py
+++ namedframes/spark_frames.py
@@ -13,3 +13,12 @@ class SparkNamedFrame(SparkDataFrame, ValidateMixIn):
             SQLContext(spark_session.sparkContext, spark_session),
         )
         self._validate()
+
+    def __repr__(self):
+        message = "DataFrame with columns:\n"
+        column_descriptions = [
+            f"{column_name}: {column_type}"
+            for column_name, column_type in self.__annotations__.items()
+        ]
+        message += "\n".join(column_descriptions)
+        return message + "\n" + super().__repr__()
